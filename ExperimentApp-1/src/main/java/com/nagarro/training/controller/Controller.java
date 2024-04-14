1de96935f1b5f7f1e3e3fbc68f4bad4f69077ed9
Hitain kakkar <64975064+HitainKakkar@users.noreply.github.com>
2024-04-14 14:08:47 +0530

    Add files via upload

---
+++ ExperimentApp-1/src/main/java/com/nagarro/training/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.nagarro.training.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/get")
+	public String TestApi() {
+		return "Hello this is Springboot API";
+	}
+	
+}
